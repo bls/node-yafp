bb42429a8f8613e4e4c1b1137cb828a20f4a2e8e
Blair Strang <Blair.Strang@auspost.com.au>
2016-01-28 10:13:38 +1100

    Remove obsolete test.

--- test/proxy_test.ts
+++ test/proxy_test.ts
@@ -160,38 +160,4 @@ describe('proxy', function() {
             done();
         });
     });
-
-
-    /*
-    it('should be possible to modify responses', function(done) {
-        proxy.use(function(clientReq, clientRes, next) {
-            proxy.forward(clientReq, c lientRes, function(proxyReq) {
-                proxyReq.on('response', function (serverResponse) {
-                    serverResponse.on('data', function(data) {
-                        assert.equal(data.toString(), 'CHANGEME');
-                        serverResponse.headers['content-length'] = 6;
-                        clientRes.writeHead(serverResponse.statusCode, serverResponse.headers);
-                        clientRes.end('WALRUS');
-                    });
-                });
-                proxyReq.on('error', function () {
-                    clientRes.end();
-                });
-            });
-        });
-        request({
-            method: 'GET',
-            proxy: 'http://localhost:30002',
-            url: 'http://localhost:30000/response_modify',
-            followRedirect: false
-        }, function(err, body, response) {
-            if (err) {
-                return done(err);
-            }
-            assert.equal(response.statusCode, 200);
-            assert.equal(body, 'WALRUS');
-            done();
-        });
-    });
-    */
 });
